ae9f8d63955be747b6a813e7be771ccbfad5f58c
Miko Hadikusuma <miko.hadikusuma@ni.com>
2022-11-09 16:11:44 -0600

    Create a WebView2 control

--- WinFormsApp/Form.Designer.cs
+++ WinFormsApp/Form.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.MyControl = new WinFormsControls.MyControl();
+            this.WebView2Control = new WinFormsControls.WebView2Control();
             this.SuspendLayout();
             // 
             // MyControl
@@ -41,11 +42,19 @@
             this.MyControl.TabIndex = 0;
             this.MyControl.TextBoxText = "Type here...";
             // 
+            // WebView2Control
+            // 
+            this.WebView2Control.Location = new System.Drawing.Point(345, 21);
+            this.WebView2Control.Name = "WebView2Control";
+            this.WebView2Control.Size = new System.Drawing.Size(516, 495);
+            this.WebView2Control.TabIndex = 1;
+            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(909, 556);
+            this.Controls.Add(this.WebView2Control);
             this.Controls.Add(this.MyControl);
             this.Name = "Form";
             this.Text = "Form1";
@@ -56,6 +65,7 @@
         #endregion
 
         private WinFormsControls.MyControl MyControl;
+        private WinFormsControls.WebView2Control WebView2Control;
     }
 }
 
